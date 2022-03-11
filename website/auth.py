80644f287545e081b4bbd6b21ff0b2001c4532cc
sabarthazo <sabarthazo@gmail.com>
2022-03-11 18:18:27 -0500

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -51,8 +51,7 @@ def sign_up():
         elif len(email) < 4:
             flash("Email is invalid.", category='error')
         else:
-            new_user = User(email=email, username=username, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
