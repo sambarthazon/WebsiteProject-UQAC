c8b6c27d168fd1a33c974e7adcd21ab90fdc6328
sabarthazo <sabarthazo@gmail.com>
2022-03-11 18:18:29 -0500

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from .models import Post, User, Comment, Like
-from .. import db
+from . import db
 
 views = Blueprint("views", __name__)
 
