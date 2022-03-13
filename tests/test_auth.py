ddcc703db4ee178eefcd11d1972565ff55e34e06
sabarthazo <sabarthazo@gmail.com>
2022-03-12 22:09:28 -0500

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -13,3 +13,10 @@ def test_request_signup(client):
     response = client.get('/sign-up')
     assert response.status_code == 200
     assert response.data.decode('utf-8') == 'Sign up page'
+    
+    
+@pytest.fixture(scope='module')
+def test_request_logout(client):
+    response = client.get('/logout')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Logout page'
