00c0b3a587ee3c52fdec365a455a8bce29de27b6
sabarthazo <sabarthazo@gmail.com>
2022-03-13 15:35:56 -0400

    Update users.py

--- website/users.py
+++ website/users.py
@@ -1,5 +1,6 @@
 from calendar import c
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import current_user
 from website.models import User
 from website import db
 from flask_login import login_required
@@ -8,38 +9,39 @@ from flask_login import login_required
 users = Blueprint("users", __name__)
 
 
-@users.route("/list-user")
+@users.route('/list/')
 @login_required
 def index():
     user = User.query.all()
     return render_template('list_users.html', user=user)
 
 
-@users.route("/show-user")
+@users.route('/show/<int:user_id>')
 @login_required
 def show(user_id):
     user = User.query.get(user_id)
     print(user)
-    return render_template("show_user.html", user=user)
+    return render_template('show_user.html', user=user)
 
 
-@users.route("/update-user")
+@users.route('/update')
 def update(user_id):
     user = User.query.get_or_404(user_id)
     if request.method == 'POST':
         user.username = request.form['username']
         user.email = request.form['email']
         db.session.commit()
-        flash("User" + user.username + "has been updated", category="success")
+        flash("User" + user.username + "has been updated", category='success')
         return redirect(url_for('users.list-user'))
-    return render_template("update_user.html")
+    return render_template('update_user.html')
 
 
-@users.route("/destroy-user")
+
+@users.route('/destroy')
 @login_required
 def destroy(user_id):
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
-    flash("User has been deleted!", category="success")
+    flash("User has been deleted!", category='success')
     return redirect(url_for('users.show-user'))
