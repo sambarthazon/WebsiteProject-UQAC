adf20ec25491eca491d956ed5a6267b30429793f
sabarthazo <sabarthazo@gmail.com>
2022-03-13 14:01:09 -0400

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -17,11 +17,13 @@ def create_app():
     db.init_app(app) # Application's database initialisation
     migrate.init_app(app, db)
 
+    from .users import users
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(users, url_prefix="/")
 
     from .models import User, Post, Comment, Like
 
