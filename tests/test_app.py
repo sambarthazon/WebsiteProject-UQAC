50c2d9631c261d6f1faccdab1642c7402ccb2230
sabarthazo <sabarthazo@gmail.com>
2022-03-12 21:39:45 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,14 +7,17 @@ def test_app():
     return create_app()
 
 
+@pytest.fixture(scope='module')
 def test_app_is_created(app):
     assert app.name == 'app'
     
     
+@pytest.fixture(scope='module')
 def test_config_is_load(config):
     assert config["DEBUG"] is True
     
 
+@pytest.fixture(scope='module')
 def test_request_returns_404(client):
     assert client.get("/url_does_not_exist").status_code == 404
     
