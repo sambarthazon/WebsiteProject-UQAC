bb4bcdda6bfd77b6df5a4542b550fc00c184d2e7
sabarthazo <sabarthazo@gmail.com>
2022-03-19 14:33:21 -0400

    Update test_models.py

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,13 +1,27 @@
-from website.models import User
+from website.models import User, Post, Comment, Like
 
 
 def test_new_user():
-    """
-    GIVEN a User model
-    WHEN a new User is created
-    THEN check the email, hashed_password, and role fields are defined correctly
-    """
-    user = User('patkennedy79@gmail.com', 'FlaskIsAwesome')
+    user = User(email='patkennedy79@gmail.com', username='patoche', password='FlaskIsAwesome')
     assert user.email == 'patkennedy79@gmail.com'
-    assert user.hashed_password != 'FlaskIsAwesome'
-    assert user.role == 'user'
+    assert user.username != 'Sam'
+    assert user.password == 'FlaskIsAwesome'
+    
+    
+def test_new_post():
+    post = Post(text='azert', author='patoche')
+    assert post.text == 'azert'
+    assert post.author == 'patoche'
+    
+    
+def test_new_comment():
+    comment = Comment(text='abc', author='patoche', post_id='123')
+    assert comment.text == 'abc'
+    assert comment.author != 'Victorleplusbeau'
+    assert comment.post_id == '123'
+    
+    
+def test_new_like():
+    like = Like(author='patoche', post_id='2')
+    assert like.author == 'patoche'
+    assert like.post_id != '3'
