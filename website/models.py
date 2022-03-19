16d4b1505c863cec89790ba5cdfd88b7ea5ff24f
sabarthazo <sabarthazo@gmail.com>
2022-03-19 14:33:30 -0400

    Update models.py

--- website/models.py
+++ website/models.py
@@ -10,6 +10,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    role = db.Column(db.String(150), default='reader')
     # promute = db.Column(db.Boolean(), default=False, nullable=True)
     posts = db.relationship('Post', backref='user', passive_deletes=True)
     comments = db.relationship('Comment', backref='user', passive_deletes=True)
