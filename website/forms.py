4d2c179dc5a0476d1e6ea6f720939c3fde306ff7
sabarthazo <sabarthazo@gmail.com>
2022-03-20 17:21:42 -0400

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -4,6 +4,7 @@ from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 
+# Need for a post
 class PostForm(FlaskForm):
     text = TextAreaField('Text', validators=[DataRequired()])
     submit = SubmitField('Post')
