b098de398a6d8bb5527a1d70432323633b81d20f
sabarthazo <sabarthazo@gmail.com>
2022-03-19 20:35:54 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,23 @@
-from flask import Flask
-from flask_testing import TestCase
-from sqlalchemy import true
+import pytest
+from website import create_app
 
 
-class MyTest(TestCase):
-    def create_app(self):
-        app = Flask(__name__)
-        app.config['TESTING'] = True
-        
-        return app
-    
+
+@pytest.fixture(scope="module")
+def app():
+    return create_app()
+
+
+@pytest.fixture(scope="module")
+def config():
+    assert config["DEBUG"] is True
+
+
+# @pytest.fixture(scope='module')
+# def test_client():
+#     flask_app = create_app()
+#     # Create a test client using the Flask application configured for testing
+#     with flask_app.test_client() as testing_client:
+#         # Establish an application context
+#         with flask_app.app_context():
+#             yield testing_client  # this is where the testing happens!
