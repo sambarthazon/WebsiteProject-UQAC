22b436c2687ccb98f1529603dc2e59116d5c808f
sabarthazo <sabarthazo@gmail.com>
2022-03-20 17:06:11 -0400

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -1,9 +1,10 @@
-from flask_login import current_user
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+# from flask_login import current_user
+# from flask_wtf import FlaskForm
+# from wtforms import TextAreaField, SubmitField
+# from wtforms.validators import DataRequired
 
 
-class PostForm(FlaskForm):
-    text = TextAreaField('Text', validators=[DataRequired()])
-    submit = SubmitField('Post')
+
+# class PostForm(FlaskForm):
+#     text = TextAreaField('Text', validators=[DataRequired()])
+#     submit = SubmitField('Post')
