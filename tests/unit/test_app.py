969255498bb3d57f4da0d7691a4f17bf023e7f84
sabarthazo <sabarthazo@gmail.com>
2022-03-19 20:35:56 -0400

    Create test_app.py

---
+++ tests/unit/test_app.py
@@ -0,0 +1,13 @@
+import config
+
+
+def test_app_is_created(app):
+    assert app.name == 'website'
+    
+    
+def test_config_is_loaded(config):
+    assert config["DEBUG"] is True
+
+
+def test_request_returns_404(client):
+    assert client.get("/url_does_not_exists").status_code == 404
