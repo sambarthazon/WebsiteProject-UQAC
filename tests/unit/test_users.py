28373bbbcb021e947c17e97f0f1fa1efc4b8f63d
sabarthazo <sabarthazo@gmail.com>
2022-03-20 17:21:40 -0400

    Update test_users.py

--- tests/unit/test_users.py
+++ tests/unit/test_users.py
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 from website import create_app
+=======
+>>>>>>> newDev
 from website.models import User
 from tests.conftest import create_app
 
@@ -10,6 +13,10 @@ user = User(id=1, email="patoche@gmail.com", username="patoche")
 def test_list_user():
     with flask_app.test_client() as test_client:
         response = test_client.get('/users/list')
+<<<<<<< HEAD
         assert response.status_code == 302
+=======
+        assert response.status_code == 200
+>>>>>>> newDev
         assert b"User's informations" not in response.data
     
