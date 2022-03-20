fe247eeffa54ebf58421fe76adf89f37aadfdc4f
sabarthazo <sabarthazo@gmail.com>
2022-03-19 20:36:02 -0400

    Create test_users.py

---
+++ tests/unit/test_users.py
@@ -0,0 +1,15 @@
+from website import create_app
+from website.models import User
+from tests.conftest import create_app
+
+
+flask_app = create_app()
+user = User(id=1, email="patoche@gmail.com", username="patoche")
+
+
+def test_list_user():
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/users/list')
+        assert response.status_code == 302
+        assert b"User's informations" not in response.data
+    
